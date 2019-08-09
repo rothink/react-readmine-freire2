53c13ee0b31c6a87a0d8731fb7f5ef2366e78056
rodrigoal <raraujol@indracompany.com>
2019-08-09 17:31:31 -0300

    Freireboard

--- src/App.js
+++ src/App.js
@@ -10,13 +10,8 @@ class App extends Component {
     render() {
         return (
             <div> 
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"/>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"/>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-                <div class="">
-                    <div class="row">
-                        <ListDesenv />
-                    </div>
+                <div class="row">
+                    <ListDesenv />
                 </div>
             </div>
         );
