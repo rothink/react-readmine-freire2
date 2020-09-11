0435f0b06cb5e46ad6af28271152a13730fbe70f
Alan Weber Delmonde Machado <alandelmondes@gmail.com>
2020-09-11 11:34:26 -0300

    Atualização de Sprint

--- src/components/ListDesenv.js
+++ src/components/ListDesenv.js
@@ -57,7 +57,7 @@ class ListDesenv extends Component {
     this.getMembers(this.props.sprint);
   }
 
-  getMembers(sprintId = 564) {
+  getMembers(sprintId = 644) {
     let desenvs = [];
     let url = "http://localhost/react-readmine-freire2/api-members.php";
 
